d0b14cb51e9e8ca8908372985ed627acae5390ec
donghquinn <ehdgus1524@gmail.com>
2024-01-09 10:44:16 +0900

    wip

--- basic/arrayConvert.js
+++ basic/arrayConvert.js
@@ -7,8 +7,7 @@
  * @param {*} arr : Array<number> - 정수 수열
  * @returns 50보다 크거나 같은 짝수는 2로 나누고, 50 이하의 홀수는 2를 곱한 정수수열
  */
-const solution = ( arr ) =>
-{
+const solution = ( arr ) => {
     return arr.map( ( item ) => {
         if ( item >= 50 && item % 2 === 0 ) return item / 2;
         if ( item < 50 && item % 2 !== 0 ) return item * 2;
@@ -17,4 +16,4 @@ const solution = ( arr ) =>
 };
 
 // 다른 분 코드
-const solution2=a=>a.map(v=>v>=50&&v%2==0?v/2:v<50&&v%2==1?v*2:v)
+const solution2 = a => a.map( v => v >= 50 && v % 2 == 0 ? v / 2 : v < 50 && v % 2 == 1 ? v * 2 : v );
